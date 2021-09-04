d5da05a562334904c2c1329b51526d412e120306
sotatek <sotatek@sotateks-MacBook-Pro.local>
2021-09-05 00:37:05 +0700

    change project to maven

--- actions/commons/GlobalConstant.java
+++ actions/commons/GlobalConstant.java
@@ -3,6 +3,6 @@ package commons;
 public class GlobalConstant {
 	public static final String LOGIN_URL = "http://demo.guru99.com/v4/";
 	public static final String REGISTER_URL = "http://demo.guru99.com";
-	public static final String USERID = "mngr330605";
-	public static final String PASSWORD = "pEtUtUg";
+	public static final String USERID = "mngr351622";
+	public static final String PASSWORD = "ugEmemA";
 }
